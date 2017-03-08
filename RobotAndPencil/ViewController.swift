372e335368b2e3b4153142574d9edf49656610c4
Kaleb Riley <kaleb.riley@mapquest.com>
2017-03-08 13:20:06 -0700

    Initial Commit

---
+++ RobotAndPencil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RobotAndPencil
+//
+//  Created by Kaleb Riley on 3/8/17.
+//  Copyright © 2017 Kaleb Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
